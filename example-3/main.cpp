518f4a5018c84ebe993cf85e9eee3f6a84c1c758
Svyatoslav Razmyslov <razmyslov@viva64.com>
2018-01-31 16:05:30 +0300

    Add examples.

---
+++ example-3/main.cpp
@@ -0,0 +1,8 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+int main(int argc, const char* argv[])
+{
+    if (argc != argc) {
+        return 1;
+    }
+}
---
+++ example-3/subdir/lib.cpp
@@ -0,0 +1,11 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+int foo() {
+  return 42;
+}
+
+int bar(int a, int b) {
+  return a == a;
+}
+
