4b323b7de7027dd7c838ed19e11b3ab895501eb9
Kevin Gerber <kevin.gerber@securiton.ch>
2019-09-25 11:58:50 +0200

    Add example for using a suppress base file
    Signed-off-by: Kevin Gerber <kevin.gerber@securiton.ch>

---
+++ example-8/main.cpp
@@ -0,0 +1,9 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+int main(int argc, const char* argv[])
+{
+  if (argc != argc) {
+    return 1;
+  }
+}
